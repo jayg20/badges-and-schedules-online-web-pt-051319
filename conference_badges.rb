d130f2f3779e05e45b649ae9d848e8e9693c058c
James Gomez <james.gomez320@yahoo.com>
2019-05-20 19:38:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,9 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-def batch_badge_creator
-  array = ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "MAtz"]
- assigned_array=  array.each_with_index do |name, index|
+def batch_badge_creator(attendees)
+  attendees= ["Edsgar", "Ada", "Charles", "Alan", "Grace", "Linus", "MAtz"]
+ assigned_array=  attendees.each_with_index do |name, index|
    puts "Hello, #{name}! You will be assigned to room #{index}"
    return assigned_array
  end 
